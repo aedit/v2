f5e9d10b4b9a501fae8baf481678d3bd540a7d35
Udit Sen <udisen97@gmail.com>
2021-05-14 19:50:34 +0530

    NavBar Component

---
+++ components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="nav-bar d-flex">
+    <span class="nav-bar__logo pointer">
+      <NuxtLink to="/">
+        <span class="color-primary fw-600">UDIT</span> SEN
+      </NuxtLink>
+    </span>
+    <ul class="nav-bar__links d-flex">
+      <li><NuxtLink to="/"> Home</NuxtLink></li>
+      <li><NuxtLink to="/about"> About</NuxtLink></li>
+      <li><NuxtLink to="/services"> Services</NuxtLink></li>
+      <li><NuxtLink to="/portfolio"> Portfolio</NuxtLink></li>
+      <li><NuxtLink to="/contact"> Contact</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
