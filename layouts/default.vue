4b8f0dea07a205eb4943d51dc47817cc8b73c483
Udit Sen <udisen97@gmail.com>
2021-05-14 16:20:36 +0530

    Project Initiation

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Hello',
+      link: [
+        this.checkDark
+          ? { rel: 'icon', type: 'image/x-icon', href: '/favicon-dark.png' }
+          : { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      ],
+    }
+  },
+  methods: {
+    checkDark() {
+      return window.matchMedia('(prefers-color-scheme: dark)').matches
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+html {
+  font-family: 'Poppins', 'Arial', 'Helvetica', 'Trebuchet MS', 'Verdana';
+  font-size: 40px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
