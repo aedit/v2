e22221f72285ddbc4f8ed6edc09a853a8f188452
Udit Sen <udisen97@gmail.com>
2021-05-15 14:17:30 +0530

    update: icons in footer

--- components/SocialIcons.vue
+++ components/SocialIcons.vue
@@ -1,34 +1,40 @@
 <template>
   <ul class="social-icons d-flex" :class="className">
-    <li>
-      <a href="https://facebook.com/udit.sen1" target="_blank">
-        <i class="icon icon-facebook"></i>
-      </a>
-    </li>
-    <li>
-      <a href="https://instagram.com/agreyguy" target="_blank">
-        <i class="icon icon-instagram"></i>
-      </a>
-    </li>
-    <li>
-      <a href="https://twitter.com/aeditme" target="_blank">
-        <i class="icon icon-twitter"></i>
-      </a>
-    </li>
-    <li>
-      <a href="https://github.com/aedit" target="_blank">
-        <i class="icon icon-github"></i>
-      </a>
-    </li>
-    <li>
-      <a href="https://linkedin.com/in/aedit" target="_blank">
-        <i class="icon icon-linkedin"></i>
+    <li v-for="item in list" :key="item.name">
+      <a :href="item.link" target="_blank">
+        <i :class="'icon-' + item.name"></i>
       </a>
     </li>
   </ul>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      list: [
+        {
+          name: 'facebook',
+          link: 'https://facebook.com/udit.sen1',
+        },
+        {
+          name: 'instagram',
+          link: 'https://instagram.com/agreyguy',
+        },
+        {
+          name: 'github',
+          link: 'https://github.com/aedit',
+        },
+        {
+          name: 'linkedin',
+          link: 'https://linkedin.com/in/aedit',
+        },
+        {
+          name: 'twitter',
+          link: 'https://twitter.com/aeditme',
+        },
+      ],
+    }
+  },
   computed: {
     className() {
       return this.$route.path === '/contact' ? 'enlarge' : ''
