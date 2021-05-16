ea1ea9d7a6bf5313695535c73783799f97d7f019
Udit Sen <udisen97@gmail.com>
2021-05-17 03:54:13 +0530

    enhanc: Buy me a coffee button on home

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,14 @@
       <h2 class="color-primary fw-400">Hello, I'm a</h2>
       <h1 class="home__content__head">Front-End Web Developer</h1>
       <h4 class="mb-6 fw-400">with 1 year of experience</h4>
-      <button
-        class="home__content__action border-primary px-9 py-1 rounded fw-400"
-      >
-        Let's Talk
-      </button>
+      <div class="home__content__action">
+        <button class="border-primary px-9 py-1 rounded fw-400 mr-2">
+          Let's Talk
+        </button>
+        <button class="border-primary px-6 py-1 rounded fw-400">
+          Buy Me a coffee
+        </button>
+      </div>
     </div>
     <img
       class="home__image"
