711efc12e6090e9ea22ac8319959924b642ce537
Udit Sen <udisen97@gmail.com>
2021-05-17 01:18:23 +0530

    improv: About Page

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="about">
     <div class="about__links">
-      <NuxtLink to="/about">Me</NuxtLink>
-      <NuxtLink to="/about/work">Work</NuxtLink>
-      <NuxtLink to="/about/skills">Skills</NuxtLink>
-    </div>
-    <div class="about__content">
-      <router-view class="about__route"></router-view>
+      <ul>
+        <NuxtLink to="/about"><li>Me</li></NuxtLink>
+        <NuxtLink to="/about/work"><li>Work</li></NuxtLink>
+        <NuxtLink to="/about/skills"><li>Skills</li></NuxtLink>
+      </ul>
       <button
         class="download-resume pointer px-6 py-1 rounded fw-400 border-primary"
         @click="openResume"
       >
-        <span>
-          <i class="icon-arrow-down"></i>
-        </span>
+        <i class="icon-arrow-up"></i>
+
         Resume
       </button>
+    </div>
+    <div class="about__content">
+      <router-view class="about__route"></router-view>
     </div>
   </div>
 </template>
