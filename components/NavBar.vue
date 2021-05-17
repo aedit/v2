240704efc1f0e31192ac0a1deeda7f5eaf5f08ff
Udit Sen <udisen97@gmail.com>
2021-05-18 03:58:11 +0530

    enhance: Dir centered selector moved to NavBar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -6,10 +6,11 @@
       </NuxtLink>
     </span>
     <ul class="nav-bar__links">
-      <li><NuxtLink class="home-link" to="/"> Home</NuxtLink></li>
+      <li><NuxtLink to="/"> Home</NuxtLink></li>
       <li><NuxtLink to="/about"> About</NuxtLink></li>
       <li><NuxtLink to="/projects"> Projects</NuxtLink></li>
       <li><NuxtLink to="/contact"> Contact</NuxtLink></li>
+      <span ref="selected-indicator" class="selected-indicator"></span>
       <button
         class="download-resume pointer px-6 py-1 rounded fw-400 border-primary"
         @click="openResume"
@@ -23,9 +24,25 @@
 </template>
 <script>
 export default {
+  watch: {
+    $route() {
+      this.$nextTick(() => this.resetIndicator())
+    },
+  },
+  mounted() {
+    this.resetIndicator()
+  },
   methods: {
     openResume() {
       window.open('/resume.pdf', '_blank')
+    },
+    resetIndicator() {
+      const selectedIndicatorStyle = this.$refs['selected-indicator'].style
+      const parentElement = this.$el.querySelector(
+        '.nav-bar__links .nuxt-link-exact-active'
+      ).parentElement
+      selectedIndicatorStyle.left = parentElement.offsetLeft + 'px'
+      selectedIndicatorStyle.width = parentElement.clientWidth + 'px'
     },
   },
 }
