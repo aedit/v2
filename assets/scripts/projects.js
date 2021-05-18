00751313058e6ad1dd919dc3977905f6eb672623
Udit Sen <udisen97@gmail.com>
2021-05-19 02:48:33 +0530

    refactor: Projects Page structure and setup for new UI

---
+++ assets/scripts/projects.js
@@ -0,0 +1,114 @@
+export default [
+  {
+    title: 'lorem Ipsum Dolor Amser',
+    image: 'https://unsplash.it/1536/864',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, asperiores!',
+    techs: [
+      'Spotify Web API',
+      'Firebase',
+      'Next,js',
+      'Nuxt.js',
+      'Vue',
+      'Google Developer Tools',
+    ],
+    githubURL: 'https://github.com/aedit',
+    projectURL: 'https://aedit.github.io/old-portfolio',
+  },
+  {
+    title: 'lorem Ipsum Dolor Amser',
+    image: 'https://unsplash.it/1536/864',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, asperiores!',
+    techs: [
+      'Spotify Web API',
+      'Firebase',
+      'Next,js',
+      'Nuxt.js',
+      'Vue',
+      'Google Developer Tools',
+    ],
+    githubURL: 'https://github.com/aedit',
+    projectURL: 'https://aedit.github.io/old-portfolio',
+  },
+  {
+    title: 'lorem Ipsum Dolor Amser',
+    image: 'https://unsplash.it/1536/864',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, asperiores!',
+    techs: [
+      'Spotify Web API',
+      'Firebase',
+      'Next,js',
+      'Nuxt.js',
+      'Vue',
+      'Google Developer Tools',
+    ],
+    githubURL: 'https://github.com/aedit',
+    projectURL: 'https://aedit.github.io/old-portfolio',
+  },
+  {
+    title: 'lorem Ipsum Dolor Amser',
+    image: 'https://unsplash.it/1536/864',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, asperiores!',
+    techs: [
+      'Spotify Web API',
+      'Firebase',
+      'Next,js',
+      'Nuxt.js',
+      'Vue',
+      'Google Developer Tools',
+    ],
+    githubURL: 'https://github.com/aedit',
+    projectURL: 'https://aedit.github.io/old-portfolio',
+  },
+  {
+    title: 'lorem Ipsum Dolor Amser',
+    image: 'https://unsplash.it/1536/864',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, asperiores!',
+    techs: [
+      'Spotify Web API',
+      'Firebase',
+      'Next,js',
+      'Nuxt.js',
+      'Vue',
+      'Google Developer Tools',
+    ],
+    githubURL: 'https://github.com/aedit',
+    projectURL: 'https://aedit.github.io/old-portfolio',
+  },
+  {
+    title: 'lorem Ipsum Dolor Amser',
+    image: 'https://unsplash.it/1536/864',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, asperiores!',
+    techs: [
+      'Spotify Web API',
+      'Firebase',
+      'Next,js',
+      'Nuxt.js',
+      'Vue',
+      'Google Developer Tools',
+    ],
+    githubURL: 'https://github.com/aedit',
+    projectURL: 'https://aedit.github.io/old-portfolio',
+  },
+  {
+    title: 'lorem Ipsum Dolor Amser',
+    image: 'https://unsplash.it/1536/864',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, asperiores!',
+    techs: [
+      'Spotify Web API',
+      'Firebase',
+      'Next,js',
+      'Nuxt.js',
+      'Vue',
+      'Google Developer Tools',
+    ],
+    githubURL: 'https://github.com/aedit',
+    projectURL: 'https://aedit.github.io/old-portfolio',
+  },
+]
