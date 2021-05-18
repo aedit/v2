419a466b7c555c45f1e735ee2247c70e0b4a1b88
Udit Sen <udisen97@gmail.com>
2021-05-19 01:40:48 +0530

    enhanc: ScrollText for all pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,11 @@
   <div class="def-layout">
     <NavBar />
     <Nuxt />
-    <span v-if="showScrollText" class="scroll-down">
-      <i class="icon-arrow-left mr-2"></i> scroll down</span
-    >
+    <span class="scroll-down">
+      <i v-if="scrollText.down" class="icon-arrow-left mr-2"></i>
+      <span class="px-2">scroll</span>
+      <i v-if="scrollText.up" class="icon-arrow-right ml-2"></i>
+    </span>
     <footer>
       <SocialIcons />
     </footer>
@@ -32,8 +34,21 @@ export default {
     }
   },
   computed: {
-    showScrollText() {
-      return this.$route.path === '/' || this.$route.path === '/projects'
+    scrollText() {
+      const scrollText = {}
+      switch (this.$route.path) {
+        case '/':
+          scrollText.down = true
+          break
+        case '/projects':
+        case '/about':
+          scrollText.down = true
+          scrollText.up = true
+          break
+        case '/contact':
+          scrollText.up = true
+      }
+      return scrollText
     },
   },
   methods: {
@@ -56,7 +71,7 @@ export default {
     display: flex;
     align-items: center;
 
-    i {
+    .icon-arrow-left {
       position: absolute;
       animation: move-down 1.5s infinite;
       left: -1rem;
@@ -71,6 +86,25 @@ export default {
       }
       100% {
         transform: translate(-20px, 0);
+        opacity: 0;
+      }
+    }
+
+    .icon-arrow-right {
+      position: absolute;
+      animation: move-up 1.5s infinite;
+      right: -1rem;
+    }
+    @keyframes move-up {
+      0% {
+        transform: translate(0, 0);
+        opacity: 0;
+      }
+      50% {
+        opacity: 1;
+      }
+      100% {
+        transform: translate(20px, 0);
         opacity: 0;
       }
     }
