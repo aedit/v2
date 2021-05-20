b89c2d9272f73ce358086564a4e883a7873d4815
Udit Sen <udisen97@gmail.com>
2021-05-21 01:10:48 +0530

    Added Error Page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,10 @@
 <template>
-  <div></div>
+  <div class="error">
+    <h1>404</h1>
+    <p>Are you lost?</p>
+    <p>Cause this page doesnt exist!</p>
+    <span class="mt-6">
+      Go back to <NuxtLink class="color-primary" to="/">Home</NuxtLink>
+    </span>
+  </div>
 </template>
