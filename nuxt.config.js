41954f617fbe7e88e6b2044f160e4c6b0fbd2b46
Udit Sen <udisen97@gmail.com>
2021-05-28 01:01:49 +0530

    google analytics added

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,13 @@ export default {
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap',
+        async: true,
+      },
+    ],
+    script: [
+      {
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-LT03TLYBCG',
+        async: true,
       },
     ],
   },
@@ -29,7 +36,10 @@ export default {
   css: ['@/assets/styles/main.scss', '@/assets/icons/style.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/vue-touch', ssr: false }],
+  plugins: [
+    { src: '~/plugins/vue-touch', ssr: false },
+    { src: '~/plugins/google-analytics', ssr: false },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
