ade0632d4e209f256be1c6eea4a07b1bfb111c10
Udit Sen <udisen97@gmail.com>
2021-05-29 02:25:27 +0530

    title changed for nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '%s Udit Sen',
+    title: 'Udit Sen',
     htmlAttrs: {
       lang: 'en',
     },
